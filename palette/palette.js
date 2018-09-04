4fecebacb3c70aeaccf53c0cf173008f09d90cb4
Ilian Bronchart <>
2018-09-05 00:03:52 +0200

    0.9 - Added a palette feature + bugfixes

---
+++ palette/palette.js
@@ -0,0 +1,15 @@
+class Palette{
+    constructor(){
+        this.colors = null;
+        this.width = null;
+        this.height = null;
+    }
+
+    add_color(){
+        
+    }
+
+    remove_color(){
+
+    }
+}
