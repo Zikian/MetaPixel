bc4bb42e9bf3c429414b3f567e4d677b77566236
Ilian Bronchart <>
2018-10-29 18:33:58 +0100

    Added open image button (PNG/GIF/JPG)

--- header/header.js
+++ header/header.js
@@ -59,3 +59,30 @@ document.getElementById("flip-horizontal").onclick = function(){
 document.getElementById("flip-vertical").onclick = function(){
     state.selection.flip("vertical");
 }
+
+document.getElementById("file-input").onchange = function(){
+    var file = event.target.files[0];
+    if(!file) { return; }
+    if(file.type != "image/png" && file.type != "image/jpeg" && file.type != "image/gif"){
+        alert("Incorrect filetype (PNG / JPG / GIF)")
+        return;
+    }
+
+    var reader = new FileReader();
+    reader.onload = function(){
+        var img = new Image();
+        img.onload = function(){
+            init("single-image", this.width, this.height, 0, 0, true, file.name);
+            state.current_layer.imageSmootingEnabled = false;
+            state.current_layer.render_ctx.drawImage(this, 0, 0);
+            state.canvas_handler.redraw_background();
+            state.canvas_handler.render_drawing();
+            state.preview_canvas.redraw();
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(file);
+}
+document.getElementById("open-image-button").onclick = function(){
+    document.getElementById("file-input").click();
+}
